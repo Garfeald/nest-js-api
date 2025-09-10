187fac6080a4684ccc551440b62478120e4de417
Konstantin Naumov <garfeeeld@mail.ru>
2025-09-10 21:41:23 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+
+
+@Module({
+    controllers: [AppController],
+    providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            database: 'nest-js-api',
+            models: [],
+            autoLoadModels: true
+        }),
+    ],
+})
+export class AppModule {
+
+}
